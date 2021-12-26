54c56def71207727fc9a7b6bf64b4fd8172e2ef1
Franco Piccirilli <francopiccirilli51@gmail.com>
2021-12-26 03:13:33 -0300

    Primera Actualizacion

---
+++ pages/api/gameDefault.js
@@ -0,0 +1,20 @@
+export default function handler (req,res) {
+    res.json([
+        {
+            "primerTexto": "¿QueHace?",
+            "segundoTexto": "Es un juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas.",
+        },
+        {
+            "primerTexto": "¿QueHace?",
+            "segundoTexto": "Es un juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas.",
+        },
+        {
+            "primerTexto": "¿QueHace?",
+            "segundoTexto": "Es un juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas.",
+        },
+        {
+            "primerTexto": "¿QueHace?",
+            "segundoTexto": "Es un juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas.",
+        },
+    ])
+}
