0d8497d2ffe2a563a62e9751b9241cb42b3d4c3f
Franco Piccirilli <francopiccirilli51@gmail.com>
2021-12-26 03:31:49 -0300

    Primer Actualizacion

--- README.md
+++ README.md
@@ -1,34 +1,18 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# ¿QueHace? - Web Game
+¿QueHace? es un juego on-line, basado en el famoso juego de cartas "A.T.R".
+Su objetivo es entretener al publico con oraciones graciosas.
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
+Para clonar el repositorio:
+```
+git clone https://github.com/breakerjs/quehace-game.git
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Comandos a correr en la terminal al iniciar el proyecto:
+```
+npm i
+npm run dev
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+```
+El ```npm run dev``` se utilizara para correr el proyecto en modo desarrollo.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Hecho por Franco Piccirilli.
--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,22 @@ export default function Home({ tiradaCarta1, tiradaCarta2 }) {
     <div>
       <Head>
         <title>¿QueHace?</title>
-        <meta name="description" content="Juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas." />
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap" rel="stylesheet"/>
+        <meta name='description' content='Juego en el que se lanzan cartas randomizadas, y se forman oraciones graciosas.' />
+        <link rel='icon' href='/favicon.ico' />
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossOrigin='anonymous'></link>
+        <link rel='preconnect' href='https://fonts.googleapis.com'/>
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true'/>
+        <link href='https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap' rel='stylesheet'/>
       </Head>
       <main>
         <div className='container-fluid bg-light mt-5'>
           <br/><br/>
           <h1 className='text-center text-muted' id='tituloPagina'>¿QueHace?</h1>
-          <h5 className='text-center text-muted' id='descripcionPagina'>¿QueHace? es un juego on-line, basado en el famoso juego de cartas "A.T.R". <br/> Su objetivo es entretener al publico con oraciones graciosas.</h5>
+          <h5 className='text-center text-muted' id='descripcionPagina'>¿QueHace? es un juego on-line, basado en el famoso juego de cartas A.T.R. <br/> Su objetivo es entretener al publico con oraciones graciosas.</h5>
           <br/><br/>
         </div>
         <br/><br/>
-        <div className="tiradaCartas d-flex justify-content-center">
+        <div className='tiradaCartas d-flex justify-content-center'>
           <button className='btn btn-secondary' onClick={(e) => {
             if(document.getElementById('texto1').style.opacity == 0) {
               document.getElementById('texto1').style.opacity = 1
@@ -38,11 +38,11 @@ export default function Home({ tiradaCarta1, tiradaCarta2 }) {
               <h3 id='texto2'>&nbsp;{tiradaCarta2}</h3>
         </div>
         <br/><br/><br/><br/><br/>
-        <nav class="navbar fixed-bottom navbar-light bg-light rounded-5">
+        <nav className='navbar fixed-bottom navbar-light bg-light rounded-5'>
           <h5 className='mx-auto text-muted'>Creado por Franco Piccirilli</h5>
         </nav>
       </main>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
+      <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM' crossOrigin='anonymous'></script>
     </div>
   )
 }
