52d3b3bede1c70205a9025a01adc3d6aaf42377c
Franco Piccirilli <francopiccirilli51@gmail.com>
2022-04-09 12:24:51 -0300

    Fix

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import {
-  server
-} from '../config/index';
 
 export default function Home({ tiradaCarta1, tiradaCarta2 }) {
   return (
