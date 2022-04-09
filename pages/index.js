d5c4b8da7df89cd8c6993260c10c18eb53b1dd0d
Franco Piccirilli <francopiccirilli51@gmail.com>
2022-04-09 12:34:46 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({ tiradaCarta1, tiradaCarta2 }) {
 }
 
 
-export async function getStaticProps () {
+export async function getServerSideProps () {
   const res = await fetch(`https://quehace-api.vercel.app/datos.json`)
   const tiradaCarta1 = await res.json()
   const random = Math.floor(Math.random() * tiradaCarta1.length)
